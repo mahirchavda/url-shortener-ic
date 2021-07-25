1e7751375ab32801d943706c69a51c7525dbe334
mahirchavda <mahir.chavda13@gmail.com>
2021-07-25 11:58:53 +0000

    Add short url generation logic

--- api/endpoints/url_shortener.py
+++ api/endpoints/url_shortener.py
@@ -1,4 +1,9 @@
+import base64
+import hashlib
 from flask_restful import Resource, reqparse
+
+SHORT_URL_LENGTH = 7
+SHORTENER_WEB_SERVER = "https://abc.com"
 
 
 class URLShortener(Resource):
@@ -7,7 +12,15 @@ class URLShortener(Resource):
         self.parser.add_argument("url", required=True, location="json")
         super().__init__()
 
+    def get_url_hash(self, url):
+        md5_hash = hashlib.md5(url.encode("utf-8")).digest()
+        url_hash = base64.urlsafe_b64encode(md5_hash).decode("ascii")
+        return url_hash[:SHORT_URL_LENGTH]
+
     def post(self):
         args = self.parser.parse_args()
-        response = {"short_url": "", "url": args["url"]}
+        url = args["url"]
+        url_hash = self.get_url_hash(url)
+        short_url = "{}/{}".format(SHORTENER_WEB_SERVER, url_hash)
+        response = {"short_url": short_url, "url": url}
         return response
--- tests/test_shorten_endpoint.py
+++ tests/test_shorten_endpoint.py
@@ -3,4 +3,7 @@ import pytest
 
 def test_shorten_endpoint_success(client):
     res = client.post("/api/shorten", json={"url": "https://google.com"})
-    assert res.json == {"short_url": "", "url": "https://google.com"}
+    assert res.json == {
+        "short_url": "https://abc.com/mZmevP2",
+        "url": "https://google.com",
+    }
