1d60134d8386ff365b3680b9ec21941ddc0b9091
mahirchavda <mahir.chavda13@gmail.com>
2021-07-25 11:23:47 +0000

    initial project setup

---
+++ api/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class URLShorten(Resource):
+    def get(self):
+        return {"test": "test"}
+
+
+api.add_resource(URLShorten, "/")
